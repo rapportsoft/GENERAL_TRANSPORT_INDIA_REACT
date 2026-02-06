2936ca64dd1efd73dc244f33fd6667cbe8eacf16
Sanket13G <sanketghodake316@gmail.com>
2026-02-06 16:08:13 +0530

    change in ip address

--- src/Components/IpAddress.js
+++ src/Components/IpAddress.js
@@ -1,4 +1,4 @@
-//  const ipaddress ='http://103.189.88.215:8080/GENERAL/';
+//  const ipaddress ='http://103.189.88.215:8080/GENERALTI/';
 
 //const ipaddress = 'http://103.240.90.122:8080/GENERAL/';
 
